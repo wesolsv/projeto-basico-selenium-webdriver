96c03b0e8525abc7e37c0b7df6f865e06cded9b4
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-07-25 20:29:30 -0300

    Feito downgrade da versao do selenium para utilizar o desiredcapabilities e executando testes utilizando o GRID

--- src/main/java/br/ce/woliver/core/DriverFactory.java
+++ src/main/java/br/ce/woliver/core/DriverFactory.java
@@ -1,41 +1,74 @@
 package br.ce.woliver.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.ce.woliver.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
-	
-	//private static WebDriver driver;
+
+	// private static WebDriver driver;
 	private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>() {
 		@Override
 		protected synchronized WebDriver initialValue() {
 			return initDriver();
 		}
 	};
-	
-	private DriverFactory() {}	
-	
+
+	private DriverFactory() {
+	}
+
 	public static WebDriver getDriver() {
 		return threadDriver.get();
 	}
-		
+
 	public static WebDriver initDriver() {
 		WebDriver driver = null;
-		switch(Propriedades.browser) {
-			case FIREFOX: driver = new FirefoxDriver(); break;
-			case CHROME: driver = new ChromeDriver(); break;
+		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			switch (Propriedades.BROWSER) {
+			case FIREFOX:
+				driver = new FirefoxDriver();
+				break;
+			case CHROME:
+				driver = new ChromeDriver();
+				break;
+			}
+		}
+		if (Propriedades.TIPO_EXECUCAO == TipoExecucao.GRID) {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+			case FIREFOX:
+				cap = DesiredCapabilities.firefox();
+				break;
+			case CHROME:
+				cap = DesiredCapabilities.chrome();
+				break;
+			}
+
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.100.191:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.err.println("Fail connection");
+				e.printStackTrace();
+			}
 		}
 		return driver;
 	}
-	
+
 	public static void killDriver() {
 		WebDriver driver = getDriver();
-		if(driver != null) {
-		driver.quit();
-		driver = null;
+		if (driver != null) {
+			driver.quit();
+			driver = null;
 		}
-		if(threadDriver != null) {
+		if (threadDriver != null) {
 			threadDriver.remove();
 		}
 	}
