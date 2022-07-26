b68170651090b3ac9feafd44a58dd374331e1e5a
Wesley Oliveira Silva <wesolsv@gmail.com>
2022-07-26 19:47:52 -0300

    Ajuste de importações no driver factory

--- src/main/java/br/ce/woliver/core/DriverFactory.java
+++ src/main/java/br/ce/woliver/core/DriverFactory.java
@@ -6,7 +6,6 @@ import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
